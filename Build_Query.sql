eb7646dd79aa70752a80939769c4672316460de3
alex w <103083020@student.swin.edu.au>
2020-11-09 09:03:02 +1100

    Task 1 Complete

--- Build_Query.sql
+++ Build_Query.sql
@@ -1,1 +1,22 @@
 --Alexandre Waymel 103083020
+
+
+
+---------------------------- TASK 1 ----------------------------
+
+/*
+    Tour(TourName, Description)
+    PRIMARY KEY (TourName)
+
+    Client(ClientID, Surname, GivenName, Gender)
+    PRIMARY KEY(ClientID)
+
+    Event(TourName,EventYear,EventMonth,EventDay,Fee)
+    PRIMARY KEY(TourName,EventYear,EventMonth,EventDay)
+    FOREIGN KEY(TourName) REFERENCES Tour
+    
+    Booking(ClientID,TourName,EventYear,EventMonth,EventDay,DateBooked,Payment)
+    PRIMARY KEY(ClientID,TourName,EventYear,EventMonth,EventDay)
+    FOREIGN KEY(ClientID) REFERENCES Client
+    FOREIGN KEY(TourName,EventYear,EventMonth,EventDay) REFERENCES Event
+*/
