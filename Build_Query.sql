06819742df86b7faafeca96e34ddf9a5f4a42090
alex w <103083020@student.swin.edu.au>
2020-11-09 09:28:04 +1100

    Task 2 Complete

--- Build_Query.sql
+++ Build_Query.sql
@@ -14,9 +14,55 @@
     Event(TourName,EventYear,EventMonth,EventDay,Fee)
     PRIMARY KEY(TourName,EventYear,EventMonth,EventDay)
     FOREIGN KEY(TourName) REFERENCES Tour
-    
+
     Booking(ClientID,TourName,EventYear,EventMonth,EventDay,DateBooked,Payment)
     PRIMARY KEY(ClientID,TourName,EventYear,EventMonth,EventDay)
     FOREIGN KEY(ClientID) REFERENCES Client
     FOREIGN KEY(TourName,EventYear,EventMonth,EventDay) REFERENCES Event
 */
+-- create DATABASE QUERY;
+use QUERY;
+
+DROP TABLE IF EXISTS Tour;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS Event;
+DROP TABLE IF EXISTS Booking; 
+
+CREATE TABLE Tour(
+    TourName    NVARCHAR(100),
+    Description NVARCHAR(500),
+
+    PRIMARY KEY (TourName));
+
+CREATE TABLE Client(
+    ClientID     INT,
+    Surname      NVARCHAR(100) NOT NULL,
+    GivenName    NVARCHAR(100) NOT NULL,
+    Gender       NVARCHAR(1) CHECK (Gender IN ('M','F','I')) NULL,
+
+    PRIMARY KEY(ClientID));
+
+CREATE TABLE Event(
+TourName    NVARCHAR(100),
+EventYear   INT CHECK (len(EventYear) = 4),
+EventMonth  NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+EventDay    INT CHECK (EventDay >= 1 AND EventDay <= 31),
+Fee         MONEY CHECK (Fee > 0) NOT NULL,
+
+PRIMARY KEY(TourName,EventYear,EventMonth,EventDay),
+FOREIGN KEY(TourName) REFERENCES Tour);
+
+CREATE TABLE Booking(
+ClientID    INT,
+TourName    NVARCHAR(100),
+EventYear   INT CHECK (len(EventYear) = 4),
+EventMonth  NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+EventDay    INT CHECK (EventDay >=1 AND EventDay <=31),
+DateBooked  DATE NOT NULL,
+Payment     MONEY CHECK (Payment > 0) NOT NULL,
+
+PRIMARY KEY(ClientID,TourName,EventYear,EventMonth,EventDay),
+FOREIGN KEY(ClientID) REFERENCES Client,
+FOREIGN KEY(TourName,EventYear,EventMonth,EventDay) REFERENCES Event);
+
+
